599dacb64867d9a1e0dbcc37bf4656fe07a79fd0
Aline R <alineromano05@gmail.com>
2023-06-28 11:09:15 -0300

    analisando as variáveis

---
+++ rascunho_testes.py
@@ -0,0 +1,7 @@
+from base_limpa import df #importando a base limpa
+
+#df.info() # analisando tipo e quantidade das variáveis
+
+describe = df.describe() # analisando variáveis quanti
+print(describe)
+
