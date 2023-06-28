42f247ee88b48f6efe6eb50c44c4438d281913ca
Aline R <alineromano05@gmail.com>
2023-06-28 12:16:12 -0300

    adicionei análise das % do sono

--- rascunho_testes.py
+++ rascunho_testes.py
@@ -18,9 +18,21 @@ sample = df.sample(5) # amostra da base
 # ideia: fazer um gráfico de barras relacionando Awakenings a alguma coluna quanti, como Sleep efficiency
 
 awakenings_count = df['Awakenings'].value_counts() # contando a ocorrência de cada número
-print(awakenings_count)
+#print(awakenings_count)
 
 media_efic_awakenings = df['Sleep efficiency'].groupby(df['Awakenings']).mean() # calculando a média de eficiência para cada número
-print(media_efic_awakenings)
+#print(media_efic_awakenings)
 
 # é possível observar que pessoas que acordam 0 ou 1 vez tem eficiência do sono maior do que as demais
+
+# análise de light, deep e REM %
+soma_porcentagens = df['Light sleep percentage'][9] + df['Deep sleep percentage'][9] + df['REM sleep percentage'][9]
+print(soma_porcentagens)
+
+# essas 3 % representam as 3 fases do ciclo do sono
+# a soma das  porcentagens é de 100%. ou seja, ao aumentar uma porcentagem, outra deve diminuir para que elas sempre somem 100%.
+
+media_light = df['Light sleep percentage'].mean()
+media_deep = df['Deep sleep percentage'].mean()
+media_rem = df['REM sleep percentage'].mean()
+print(media_light, media_deep, media_rem)
