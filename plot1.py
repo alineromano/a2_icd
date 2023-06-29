de48341db7992ce057bbd2e590a2a2d76e668313
Aline R <alineromano05@gmail.com>
2023-06-28 21:10:12 -0300

    modifiquei para incluir light e REM sleep junto

--- plot1.py
+++ plot1.py
@@ -1,29 +1,39 @@
 from bokeh.io import output_file, show
 from bokeh.models import ColumnDataSource
 from bokeh.plotting import figure
-from bokeh.transform import factor_cmap
+from bokeh.transform import dodge
 from base_limpa import df
 
-# gráfico de barras Awakenings x Deep sleep percentage
+# gráfico de barras Awakenings x Light, Deep  e REM sleep percentage
+# Dessa forma, é possível analisar como é o ciclo do sono é dividido dependendo da quantidade de vezes que a pessoa acorda durante a noite
 
-# cria um dataframe em que uma coluna são as opções de awakenings e a outra são as % de deep sleep para cada uma dessas opções
-media_deep_awake = df.groupby('Awakenings')['Deep sleep percentage'].mean().reset_index() # esse cógigo agrupa o df por Awakenings e faz a média de deep sleep, depois transforma em um df
+awakenings = df['Awakenings'].unique().astype(str).tolist()
+awakenings.sort()
+categories = ['Light sleep percentage', 'Deep sleep percentage', 'REM sleep percentage']
 
-# transforma as opções de awakenings em string
-media_deep_awake['Awakenings'] = media_deep_awake['Awakenings'].astype(str) 
+data = {'awakenings': awakenings}
 
-# definindo o column data source para plotar os gráficos
-source = ColumnDataSource(media_deep_awake)
+for category in categories:
+    counts = df[category].groupby(df['Awakenings']).mean()
+    data[category] = counts.tolist()
 
+source = ColumnDataSource(data = data)
 
-plot = figure(x_range=media_deep_awake['Awakenings'].tolist())
+plot = figure(x_range=awakenings, y_range=(0, 100),
+           height=350, toolbar_location=None, tools="")
 
+plot.vbar(x=dodge('awakenings', -0.25, range=plot.x_range), top='Light sleep percentage', source=source,
+       width=0.2, color="#c9d9d3", legend_label="Light sleep percentage")
 
-plot.vbar(x='Awakenings', top='Deep sleep percentage', width=0.5, source=source,
-          fill_color=factor_cmap('Awakenings', palette=['blue', 'pink', 'orange', 'red', 'green'], factors=media_deep_awake['Awakenings'].tolist()))
+plot.vbar(x=dodge('awakenings',  0.0,  range=plot.x_range), top='Deep sleep percentage', source=source,
+       width=0.2, color="#718dbf", legend_label="Deep sleep percentage")
 
+plot.vbar(x=dodge('awakenings',  0.25, range=plot.x_range), top='REM sleep percentage', source=source,
+       width=0.2, color="#e84d60", legend_label="REM sleep percentage")
 
-plot.xaxis.axis_label = "Awakenings"
-plot.yaxis.axis_label = "Average Deep Sleep Percentage"
+plot.x_range.range_padding = 0.1
+plot.xgrid.grid_line_color = None
+plot.legend.location = "top_left"
+plot.legend.orientation = "horizontal"
 
 show(plot)
