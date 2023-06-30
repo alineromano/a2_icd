fa984df0f9452ed142525c5a517f14cff573a8c3
Aline R <alineromano05@gmail.com>
2023-06-30 15:20:57 -0300

    adicionei a função pra juntar no html

--- plot_2_ana_julia.py
+++ plot_2_ana_julia.py
@@ -6,58 +6,62 @@ import numpy as np
 from base_limpa import df
 from ICdD_modulo_base_para_cds import create_column_data_source
 
-csv_file = "Sleep_Efficiency_Limpa.csv"
-source = create_column_data_source(csv_file)
 
-# Nessa pasta, quero trabalhar com a relação entre as médias do consumo de alcóol, da porcentagem do sono profundo e do leve.
-# Talvez eu analise se a pessoa fuma ou não também. Porque acho que teria correlação com as duas variáveis iniciais.
+def create_plot8():
+    csv_file = "Sleep_Efficiency_Limpa.csv"
+    source = create_column_data_source(csv_file)
 
-# Atribuindo o nome das colunas que serão usadas à variáveis com o intuito de deixar o código mais legível
-coluna_consumo_alcool = "Alcohol consumption"
-coluna_porcentagem_sono_profundo = "Deep sleep percentage"
-coluna_porcentagem_sono_leve = "Light sleep percentage"
+    # Nessa pasta, quero trabalhar com a relação entre as médias do consumo de alcóol, da porcentagem do sono profundo e do leve.
+    # Talvez eu analise se a pessoa fuma ou não também. Porque acho que teria correlação com as duas variáveis iniciais.
 
-p = figure(title="Consumo de Álcool e Porcentagem de duração das etapas do Sono",
-           x_axis_label= "Consumo de Álcool",
-           y_axis_label= "Porcentagens",
-           width=800, height=600,
-           background_fill_color="#50C4F2",
-           background_fill_alpha=0.1)
+    # Atribuindo o nome das colunas que serão usadas à variáveis com o intuito de deixar o código mais legível
+    coluna_consumo_alcool = "Alcohol consumption"
+    coluna_porcentagem_sono_profundo = "Deep sleep percentage"
+    coluna_porcentagem_sono_leve = "Light sleep percentage"
 
-# Alterando a fonte, a cor e o tamanho da fonte do título
-p.title.text_font = "Times New Roman"
-p.title.text_color = "#50C4F2"
-p.title.text_font_size = "20pt"
+    p = figure(title="Consumo de Álcool e Porcentagem de duração das etapas do Sono",
+            x_axis_label= "Consumo de Álcool",
+            y_axis_label= "Porcentagens",
+            width=800, height=600,
+            background_fill_color="#50C4F2",
+            background_fill_alpha=0.1)
 
-# Alterando a fonte, a cor e o tamanho da fonte dos rótulos dos eixos
-p.xaxis.axis_label_text_font = "Helvetica"
-p.xaxis.axis_label_text_color = "#4C2D59"
-p.xaxis.axis_label_text_font_size = "12pt"
+    # Alterando a fonte, a cor e o tamanho da fonte do título
+    p.title.text_font = "Times New Roman"
+    p.title.text_color = "#50C4F2"
+    p.title.text_font_size = "18pt"
 
-p.yaxis.axis_label_text_font = "Helvetica"
-p.yaxis.axis_label_text_color = "#4C2D59"
-p.yaxis.axis_label_text_font_size = "12pt"
+    # Alterando a fonte, a cor e o tamanho da fonte dos rótulos dos eixos
+    p.xaxis.axis_label_text_font = "Helvetica"
+    p.xaxis.axis_label_text_color = "#4C2D59"
+    p.xaxis.axis_label_text_font_size = "12pt"
 
-# Definindo os intervalos dos eixos. Atribuí a variáveis primeiro com o intuito de deixar mais legível.
-x_range = Range1d(start=-1, end=6)
-y_range = Range1d(start=0, end=80)
-p.x_range = x_range
-p.y_range = y_range
+    p.yaxis.axis_label_text_font = "Helvetica"
+    p.yaxis.axis_label_text_color = "#4C2D59"
+    p.yaxis.axis_label_text_font_size = "12pt"
 
-# Adicionando um deslocamento aleatório aos pontos para criar o efeito de Gitter
-deslocamento = np.random.uniform(-0.7, 0.7, len(df))
-df[coluna_consumo_alcool] += deslocamento
+    # Definindo os intervalos dos eixos. Atribuí a variáveis primeiro com o intuito de deixar mais legível.
+    x_range = Range1d(start=-1, end=6)
+    y_range = Range1d(start=0, end=80)
+    p.x_range = x_range
+    p.y_range = y_range
 
-p.asterisk(df[coluna_consumo_alcool], df[coluna_porcentagem_sono_profundo],
-          legend_label="Sono Profundo", fill_color="#50C4F2", size=8, alpha=0.6)
+    # Adicionando um deslocamento aleatório aos pontos para criar o efeito de Gitter
+    deslocamento = np.random.uniform(-0.7, 0.7, len(df))
+    df[coluna_consumo_alcool] += deslocamento
 
-p.star(df[coluna_consumo_alcool], df[coluna_porcentagem_sono_leve],
-          legend_label="Sono Leve", fill_color="#C373D9", size=10, alpha=1.5)
+    p.asterisk(df[coluna_consumo_alcool], df[coluna_porcentagem_sono_profundo],
+            legend_label="Sono Profundo", fill_color="#50C4F2", size=8, alpha=0.6)
 
-# Definindo o nome do arquivo a ser exibido com o conteúdo do gráfico.
-output_file("plot_2_Ana.html", title="plot_2_Ana")
+    p.star(df[coluna_consumo_alcool], df[coluna_porcentagem_sono_leve],
+            legend_label="Sono Leve", fill_color="#C373D9", size=10, alpha=1.5)
 
-# Exibir o gráfico
-show(p)
+    # Definindo o nome do arquivo a ser exibido com o conteúdo do gráfico.
+    output_file("plot_2_Ana.html", title="plot_2_Ana")
+
+    # Exibir o gráfico
+    #show(p)
+
+    return p
 
 
