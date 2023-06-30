b055b18a9223ddaf668011736e5819e64a88e2e4
larapdalese <126460907+larapdalese@users.noreply.github.com>
2023-06-30 11:20:43 -0300

    Update plot2.py

--- plot2.py
+++ plot2.py
@@ -20,7 +20,8 @@ from ICdD_modulo_base_para_cds import create_column_data_source
 csv_file = "Sleep_Efficiency.csv"
 source = create_column_data_source(csv_file)
 
-#Nesse gráfico 1 quis mostrar que quanto maior o consumo de cafeína, menor a ocorrência de um sono profundo
+#GRÁFICO 1
+#quis mostrar que quanto maior o consumo de cafeína, menor a ocorrência de um sono profundo
 y_range_start = 0 #Colocando o mínimo dos valores do meu y-axis range
 y_range_end = 110 #Colocando o máximo dos valores do meu y-axis range
 
@@ -41,7 +42,11 @@ p.grid.grid_line_width = 1
 output_notebook()
 show(p)
 
-#GRÁFICO 2, possibilita visualizar a quantidade de vezes que as pessoas acordaram, por idade
+#GRÁFICO 2
+#possibilita visualizar a quantidade de vezes que as pessoas acordaram, dividido por idade; é possível ver que diversas pessoas 
+##com quase 40 anos relataram acordar uma vez por noite, assim como as de quase 60 também e, algumas entre 20 e 30 disseram
+###acordar 4 vezes por noite.
+
 #importa a biblioteca matplotlib.pyplot e associa ao nome "plt", pra que eu possa acessar suas funções depois 
 import matplotlib.pyplot as plt
 
@@ -74,3 +79,67 @@ plt.colorbar(label='Contagens')
 plt.title('Hexbin Plot: Vezes que acordou por idade')
 #exibe o gráfico
 plt.show()
+
+#Porém, caso o senhor queira ver meu HexbinPlot com todos os hexágonos e não apenas nas linhas que possuem respostas, aqui está
+## o código sem "mincnt".
+import matplotlib.pyplot as plt
+
+awakenings = df['Awakenings']
+age = df['Age']
+
+plt.hexbin(age, awakenings, gridsize=20, cmap='BuPu', edgecolors='#38375C')
+
+plt.xlabel('Idade')
+plt.ylabel('Vezes que despertou')
+
+plt.colorbar(label='Contagens')
+
+plt.title('Hexbin Plot: Vezes que acordou por idade')
+
+plt.show()
+
+
+#GRÁFICO 3
+#No gráfico é possível ver que pessoas fumantes tiveram uma porcentagem maior de REM sleep que os não fumantes, 
+#O que é REM Sleep? Rapid eye movement (REM) sleep é um estágio do sono em que a maior parte dos sonhos acontece. Li
+##um pouco sobre o assunto e aprendi que esse nome vêm da forma que os nossos olhos se movem enquanto estamos sonhando.
+
+# Agrupa os dados pelo estado do fumante e obtém os valores do REM sleep percentage dele
+smoking_groups = df.groupby('Smoking status')['REM sleep percentage'].apply(list)
+#Aqui eu criei o boxplot com os dados de porcentagem do REM sleep agrupados por fumante (que era classificado por 
+### respostas de "No" e "Yes"). 
+#labels: define os rótulos do eixo x, "Yes" e "No"
+# patch_artist=True: permite que eu personalize a aparência do boxplot.
+bp = plt.boxplot(smoking_groups, labels=['No', 'Yes'], patch_artist=True)
+
+#criei as variáveis que armazenariam as cores do gráfico
+background_color = '#F2E6C9' #defini a cor de fundo do gráfico
+border_color = '#CAC1B0' #escolhi a cor da borda do gráfico
+mean_line_color = '#38375C' #selecionei a cor da linha de mediana
+fill_color = '#787B93' #decidi a cor que iria preencher o boxplot
+
+#Coloca a cor de fundo do gráfico
+plt.gca().set_facecolor(background_color)
+
+#Bota a cor das bordas do boxplot
+for box in bp['boxes']:
+    box.set(color=border_color)
+    
+#Põe a cor da linha de mediana
+for median in bp['medians']:
+    median.set(color=mean_line_color)
+    
+#Define o preenchimento dos boxplots
+for box in bp['boxes']:
+    box.set(facecolor=fill_color)
+
+#Define os rótulos de x e y
+plt.xlabel('Smoking Status')
+plt.ylabel('REM Sleep Percentage')
+
+#Estabelece o título do gráfico
+plt.title('Relação entre Smoking Status e REM Sleep Percentage')
+
+# Exibe o gráfico
+plt.show()
+
